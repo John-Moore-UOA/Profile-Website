945a4792e37a55dbe60cde508cd6496a2f8677bb
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-07-08 23:39:34 +1200

    profile and headerimage layout

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,19 @@
 import ProfileNamespace from "./ProfileNamespace";
 import TopBar from "./TopBar";
+import "./headerstyles.css";
+import backgroundImage from "../assets/trail-background.jpg";
 
 function Header() {
   return (
     <>
       <TopBar />
-      <div className="profile-background-img"></div>
+      <div className="profile-background-img">
+        <img
+          src={backgroundImage}
+          className="header-background-img"
+          alt="..."
+        />
+      </div>
       <ProfileNamespace />
       {/* basic details like name location, studying, expected graduation, gpa? */}
     </>
--- src/Header/ProfileNamespace.jsx
+++ src/Header/ProfileNamespace.jsx
@@ -1,7 +1,10 @@
+import profilePicture from "../assets/profile-picture.jpg";
+
 function ProfileNamespace() {
   return (
     <>
       <div className="header-namespace">
+        <img src={profilePicture} class="img-thumbnail" alt="..." />
         <h1>John</h1>
         <p>Tauranga Engineering Software 2025 etc...</p>
       </div>
