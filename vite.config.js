44a94e56ab9d5fedff32f53d576ba49492279a4b
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-07-08 22:14:14 +1200

    reformating base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Profile-Website/",
+  base: "https://John-Moore-UOA.github.io/Profile-Website/",
   plugins: [react()],
 });
