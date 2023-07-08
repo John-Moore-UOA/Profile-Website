6548d51fab52e1efd4b26ef8886a35359915e758
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-07-08 22:34:37 +1200

    background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./styles.css";
 
+import TopBar from "./TopBar";
+
 function App() {
   return (
     <>
+      <TopBar />
       <div className="background">
-        <h1>Hello World</h1>
+        <h1 className="text-info bg-dark">Hello World</h1>
       </div>
     </>
   );
---
+++ src/TopBar.jsx
@@ -0,0 +1,9 @@
+function TopBar() {
+  return (
+    <div className="top-bar bg-dark">
+      <h1 className="text-info">John Moore Profile Website</h1>
+    </div>
+  );
+}
+
+export default TopBar;
