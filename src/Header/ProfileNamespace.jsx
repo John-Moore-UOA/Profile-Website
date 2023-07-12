8df5778f900a2fac16b913d0bb56860f0af2d835
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-07-12 16:10:30 +1200

    namespace formating

--- src/Header/ProfileNamespace.jsx
+++ src/Header/ProfileNamespace.jsx
@@ -5,46 +5,29 @@ function ProfileNamespace() {
   return (
     <>
       <div className="header-namespace">
-        <div className="row align-items-start">
-          <div className="col g-0">
-            <div className="header-namespace-img-container">
-              <img src={profilePicture} className="img-thumbnail" alt="..." />
+        <div className="header-namespace-img-container">
+          <img src={profilePicture} className="img-thumbnail" alt="..." />
+        </div>
+        <div className="header-namespace-details">
+          <div className="row">
+            <div className="col">
+              {" "}
+              <p className="namespace-name">John Moore</p>
             </div>
+            <div className="col"> </div>
           </div>
-          <div className="col g-0">
-            <div className="header-namespace-details">
-              <div className="row">
-                <div className="col">
-                  {" "}
-                  <p className="namespace-name">John Moore</p>
-                </div>
-                <div className="col">
-                  {" "}
-                  <p className="namespace-major">
-                    Bachelor Of Software Engineering at the University of
-                    Auckland
-                  </p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <p className="namespace-minor">Tauranga, New Zealand</p>
-                </div>
-                <div className="col">
-                  <p className="namespace-minor">
-                    Expected Graduation: Nov 2025
-                  </p>
-                </div>
-                <div className="col">
-                  <p className="namespace-minor">placeholder</p>
-                </div>
-                <div className="col">
-                  <p className="namespace-minor">placeholder</p>
-                </div>
-                <div className="col">
-                  <p className="namespace-minor">placeholder</p>
-                </div>
-              </div>
+          <div className="row">
+            <div className="col">
+              <p className="namespace-minor">Software Engineering</p>
+            </div>
+            <div className="col">
+              <p className="namespace-minor">Expected Graduation: Nov 2025</p>
+            </div>
+            <div className="col">
+              <p className="namespace-minor">University Of Auckland</p>
+            </div>
+            <div className="col">
+              <p className="namespace-minor">From Tauranga, New Zealand</p>
             </div>
           </div>
         </div>
