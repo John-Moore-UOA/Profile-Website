5dde5eca459926ffe68808014696b1997ff6c25f
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-07-12 22:48:24 +1200

    cards uploaded

--- src/Body/Card.jsx
+++ src/Body/Card.jsx
@@ -7,9 +7,11 @@ function Card(cardProps) {
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.description}</p>
-          <a href={props.link} className="btn btn-primary">
-            Go somewhere
-          </a>
+          {props.link && (
+            <a href={props.link} className="btn btn-primary">
+              Link
+            </a>
+          )}
         </div>
       </div>
     </>
--- src/Body/ContentSlider.jsx
+++ src/Body/ContentSlider.jsx
@@ -1,34 +1,63 @@
 import { useState } from "react";
 
 import picture_1 from "../assets/content/test_picture.jpg";
-import picture_2 from "../assets/profile-picture.jpg";
+import bf_img from "../assets/content/bfwebsite.png";
+import mandleBrot from "../assets/profile-picture.jpg";
+import assignment3_img from "../assets/content/assignment3image.png";
+import mazeMania_img from "../assets/content/mazemaniawebsite.png";
+import chess_img from "../assets/content/chesswebsite.png";
 
 import Card from "./Card";
 
 function ContentSlider() {
   let value1 = {
-    title: "One",
-    description: "Test Description 1",
-    image: picture_1,
-    link: "#",
+    title: "Bf Code Compiler",
+    description:
+      "Compiler for the Theoretical Language of Bf.  This language is essentially manual Turing machine.  The link below has a more detailed description of the language",
+    image: bf_img,
+    link: "https://github.com/John-Moore-UOA/BF-compiler",
   };
 
   let value2 = {
-    title: "Two",
-    description: "Test Description 2",
-    image: picture_2,
-    link: "#",
+    title: "MandleBrot Set in raw C",
+    description:
+      "Developed a Graphical pixel drawing application to then render the MandelBrot Set as a bitmap",
+    image: mandleBrot,
+    link: false,
   };
 
   let value3 = {
+    title: "MazeMania",
+    description:
+      "An old school maze traversal game with applying a maze generation algorithm",
+    image: mazeMania_img,
+    link: "https://github.com/John-Moore-UOA/Personal/tree/main/MazeMania-master",
+  };
+
+  let value4 = {
     title: "Three",
     description: "Test Description 3",
     image: picture_1,
     link: "#",
   };
 
+  let value5 = {
+    title: "Discrete Math Calculator",
+    description:
+      "Discrete Math Assignment 3 Graph Theory Calculator, with Depth and Breadth First Search",
+    image: assignment3_img,
+    link: "https://github.com/John-Moore-UOA/University-Projects/tree/main/assignment-3-John-Moore-UOA-main",
+  };
+
+  let value6 = {
+    title: "Chess",
+    description: "A simple C project I made to learn C a number of years ago",
+    image: chess_img,
+    link: "https://github.com/John-Moore-UOA/Personal/tree/main/Chess",
+  };
+
   const [index, setIndex] = useState(0);
-  let values = [value1, value2, value3];
+  let values = [value1, value2, value3, value4, value5, value6];
 
   const onClick = (isNext) => {
     console.log("clicked");
@@ -44,13 +73,14 @@ function ContentSlider() {
   return (
     <>
       <div className="body-content-slider">
+        <h1>Projects</h1>
         <div className="body-content-grid">
           <div
             className="display-previous"
             type="button"
             onClick={() => onClick(false)}
           >
-            Back
+            {index + 1} out of {values.length}
           </div>
           <Card cardProps={values[index]} />
           <div
@@ -58,7 +88,7 @@ function ContentSlider() {
             type="button"
             onClick={() => onClick(true)}
           >
-            Next
+            {index + 1} out of {values.length}
           </div>
         </div>
       </div>
