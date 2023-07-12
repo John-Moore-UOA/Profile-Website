8f782bc1acf49d6f65c9a232904b75dc84c1ca9c
John Moore <126381092+John-Moore-UOA@users.noreply.github.com>
2023-07-12 21:38:52 +1200

    about me sidebar

--- src/Body/SideBar.jsx
+++ src/Body/SideBar.jsx
@@ -2,7 +2,27 @@ function SideBar() {
   return (
     <>
       <div className="body-sidebar">
-        <h1>SideBar</h1>
+        <h1>About Me</h1>
+        <div className="body-sidebar-paragraph">
+          <p>
+            Hello, I am a passionate student studying Software Engineering at
+            the University of Auckland. I am activly working hard towards my
+            degree and am currently in my second year of study.
+          </p>
+
+          <p>
+            My aspiration is to contribute to high speed computation work
+            applying mathematics and physics towards the development of a real
+            world application. I like software development, as it provides both
+            a logical and creative challenge.
+          </p>
+
+          <p>
+            Java, JavaScript, C, C++, Python, Matlab, Typescript, HTML, CSS, C#
+          </p>
+
+          <h4>See my personal projects to the right of this section.</h4>
+        </div>
       </div>
     </>
   );
